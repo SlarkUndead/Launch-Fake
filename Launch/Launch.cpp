e01f4bb4ace7e099f421e760836e03f0ba6724f5
Hải Lê <dark.hades.1102@gmail.com>
2016-10-15 20:11:58 +0900

    Smt

--- Launch/Launch.cpp
+++ Launch/Launch.cpp
@@ -8,9 +8,35 @@
 */
 
 #include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
 
+string getClientVersion() {
+	string file;
+	file = "(version)";
+
+	char data[100];
+
+	ifstream v;
+	v.open(file);
+	v >> data;
+	//cout << data;
+	v.close();
+
+	return file;
+}
 int main()
 {
+	ofstream myfile("(version)");
+	if (myfile.is_open())
+	{
+		cout << "fsf";
+		myfile.close();
+	}
+	else cout << "Unable to open file";
+	system("pause");
 	return 0;
 }
